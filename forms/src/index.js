86082a838a1dee999c217b5e03c068ab022154f5
Gleb Baykov <gleb.baykov99@gmail.com>
2021-08-16 23:00:09 +0300

    init: create repo and folder "forms"

---
+++ forms/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Card, CardField, MakeCardField } from './card';
+// import { SearchBar } from './search-bar';
+// import './style.scss'
+// window.React = React;
+
+
+// const App = () => {
+//   return (
+//     <div>
+// <SearchBar/>
+// <MakeCardField/>
+//     </div>
+
+//   );
+// }
+
+// const root = document.getElementById('root');
+// ReactDOM.render(<App/>, root);
