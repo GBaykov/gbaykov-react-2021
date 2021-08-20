ed85d34b94fbc966f1946112c974435c660ad1e4
Gleb Baykov <gleb.baykov99@gmail.com>
2021-08-20 19:00:34 +0300

    feat:make dist

--- forms/src/index.js
+++ forms/src/index.js
@@ -3,24 +3,22 @@ import ReactDOM from 'react-dom';
 import { Card } from './components/Card/Card.jsx';
 import { Form } from './components/Form/form.jsx';
 
- import './style.scss'
- window.React = React;
+import './style.scss';
 
+window.React = React;
 
 export const App = () => {
-  const [formvalues, setFormValues] = useState([])
+  const [formvalues, setFormValues] = useState([]);
   return (
     <div className='App'>
       <Form setFormValues={setFormValues}/>
       <main>
-        {formvalues.map((item, idx)=>{
-          return <Card item = {item} key={idx}/>
-        })}
+        {formvalues.map((item, idx) => <Card item = {item} key={idx}/>)}
       </main>
     </div>
 
   );
-}
+};
 
 const root = document.getElementById('root');
 ReactDOM.render(<App/>, root);
